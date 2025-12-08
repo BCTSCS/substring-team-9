af8e86fdb5c981850ee1db1ba25de77c32252267
Addysaurus <addisonalmagor@gmail.com>
2025-12-08 14:03:04 -0500

    ListPractice

---
+++ ListPractice.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ListPractice {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("syntax");
+        myList.add("parser");
+        myList.add("token");
+        myList.add("lexical");
+
+        for (String i : myList) {
+            System.out.println(i);
+        }
+    
+        for (int j = 0; j < myList.size(); j ++) {
+            System.out.println("Word: '" + myList.get(j).toUpperCase() + "', Index: " + j);
+        }
+
+        for (String i : myList) {
+            if (i.length() >= 5) {
+                System.out.println(i);
+            }
+        }
+    }
+}
